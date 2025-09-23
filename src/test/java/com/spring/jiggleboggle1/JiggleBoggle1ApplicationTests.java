48ac2663b44d3694dead3007f3b144e81582d3f3
군고구마 <eunseo0704319@gmail.com>
2025-09-23 17:45:10 +0900

    “연결”

---
+++ src/test/java/com/spring/jiggleboggle1/JiggleBoggle1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jiggleboggle1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiggleBoggle1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
