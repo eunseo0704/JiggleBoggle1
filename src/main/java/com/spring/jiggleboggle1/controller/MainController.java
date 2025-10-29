1464e23fb1645d163fe74a832c0ec860d4cf6982
jinny <jinheep22@gmail.com>
2025-10-29 14:48:41 +0900

    로그인 JWT 인증 공통화 및 로그인후 layout 변경

--- src/main/java/com/spring/jiggleboggle1/controller/MainController.java
+++ src/main/java/com/spring/jiggleboggle1/controller/MainController.java
@@ -34,33 +34,8 @@ public class MainController {
     }
 
     @GetMapping("/MainPage")
-    public String mainPage(Model model, HttpServletRequest request,
-                           @CookieValue(value = "JWT_TOKEN", required = false) String token) {
-
-
-        // 2. 쿠키에서 JMT_TOKEN 가져오기
-        if (request.getCookies() != null) {
-            for (Cookie cookie : request.getCookies()) {
-                if ("JWT_TOKEN".equals(cookie.getName())) {
-                    token = cookie.getValue();
-                    break;
-                }
-            }
-        }
-
-        // 3. JWT 유효성 체크
-        if (token != null && jwtUtil.validateToken(token)) {
-            String userId = jwtUtil.getUserIdFromToken(token);
-            String userName = jwtUtil.getUserNameFromToken(token);
-
-            model.addAttribute("isLoggedIn", true);
-            model.addAttribute("userId", userId);
-            model.addAttribute("userName", userName);
-        } else {
-            model.addAttribute("isLoggedIn", false);
-        }
-
-        // 4. 뷰 반환
+    public String mainPage(Model model) {
+        // 뷰 반환
         return "main/MainPage";
     }
 
