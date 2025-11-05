fcfab7aea9f8a358b9b8efc261f1b0478bbbb2d3
jinny <jinheep22@gmail.com>
2025-11-06 00:40:09 +0900

    레시치 조회 searchName 수정

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -4,36 +4,5 @@ window.addEventListener('DOMContentLoaded',function (){
         alert(msg);
     }
 
-    const moveMainPage = document.getElementById("moveMainPage");
-    const moveIcon = document.getElementById("moveIcon");
-
-    if (moveMainPage) {
-        moveMainPage.addEventListener("click", function () {
-            window.location.href = "/MainPage"; // 메인 페이지로 이동
-        });
-    }
-
-    if (moveIcon) {
-        moveIcon.addEventListener("click", function () {
-            window.location.href = "/MainPage"; // 메인 페이지로 이동
-        });
-    }
-
-    const logoutBtn = document.getElementById("logoutBtn");
-    if (logoutBtn) {
-        logoutBtn.addEventListener("click", function () {
-            window.location.href = "/logout"; // 메인 페이지로 이동
-        });
-    }
-
-    const searchBtn = document.getElementById("searchBtn");
-    if (searchBtn) {
-        searchBtn.addEventListener("click", function () {
-            window.location.href = "/searchRecipe"; // 메인 페이지로 이동
-        });
-    }
-
-
-
 });
 
